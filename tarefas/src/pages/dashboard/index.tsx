6a5a732aec7a436ab50afd89631a0244cc6b3f79
leandrogarcia <leandro.oliveira.garcia@gmail.com>
2025-10-22 07:39:12 -0300

    fix: downgrade next to 15 version to use next-auth

---
+++ tarefas/src/pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import styles from './styles.module.css';
+
+export default function Dashboard(){
+    return(
+        <div className={styles.container}>
+            <Head>
+                <title>Meu painel de tarefas</title>
+            </Head>
+            <h1>Painel</h1>
+        </div>
+    );
+}
