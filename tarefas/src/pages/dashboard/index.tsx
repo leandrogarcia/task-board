26b64e75655ada30f1ec22df205788aba2497df0
leandrogarcia <leandro.oliveira.garcia@gmail.com>
2025-10-27 22:29:14 -0300

    feat:Add server-side authentication check and redirect to home if user is not logged in on Dashboard page

--- tarefas/src/pages/dashboard/index.tsx
+++ tarefas/src/pages/dashboard/index.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head';
 import styles from './styles.module.css';
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
+import { redirect } from 'next/dist/server/api-utils';
 
 export default function Dashboard(){
     return(
@@ -10,4 +13,24 @@ export default function Dashboard(){
             <h1>Painel</h1>
         </div>
     );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
+
+    const session = await getSession({req});
+    
+    //console.log(session)
+
+    if(!session?.user) {
+        return {
+            redirect : {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
+    return {
+        props: {},
+    }
 }
