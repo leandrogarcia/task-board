290c68c591351ceb8c48edddd834074bfc16aaa2
leandrogarcia <leandro.oliveira.garcia@gmail.com>
2025-10-27 23:05:39 -0300

    feat: Create and style the main layout for the dashboard page

--- tarefas/src/pages/dashboard/index.tsx
+++ tarefas/src/pages/dashboard/index.tsx
@@ -2,7 +2,9 @@ import Head from 'next/head';
 import styles from './styles.module.css';
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
-import { redirect } from 'next/dist/server/api-utils';
+import { Textarea } from '@/components/textarea';
+import {FiShare2} from 'react-icons/fi';
+import {FaTrash} from 'react-icons/fa';
 
 export default function Dashboard(){
     return(
@@ -10,7 +12,45 @@ export default function Dashboard(){
             <Head>
                 <title>Meu painel de tarefas</title>
             </Head>
-            <h1>Painel</h1>
+            
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        <h1 className={styles.title}>Qual sua tarefa</h1>
+
+                        <form>
+                            <Textarea 
+                                placeholder='Digite qual sua tarefa'
+                            />
+                            <div className={styles.checkboxArea}>
+                                <input type='checkbox' className={styles.checkbox} />
+                                <label>Deixar tarefa pública</label>
+                            </div>
+                            <button type='submit' className={styles.button}>Registrar</button>
+                        </form>
+                    </div>
+                </section>
+
+                <section className={styles.taskContainer}>
+                    <h1>Minhas tarefas</h1>
+                    <article className={styles.task}>
+                        <div className={styles.tagContainer}>
+                            <label className={styles.tag}>PUBLICO</label>
+                            <button className={styles.shareButton}>
+                                <FiShare2
+                                    size={22}
+                                    color='#3183ff'
+                                />
+                            </button>
+                        </div>
+                        <div className={styles.taskContent}> 
+                            <p>Minha primeira tarefa de exmplo</p>
+                            <button className={styles.trashButton}><FaTrash size={24} color='#ea3140' /></button>
+                        </div>
+                    </article>
+                    
+                </section>
+            </main>
         </div>
     );
 }
@@ -18,8 +58,6 @@ export default function Dashboard(){
 export const getServerSideProps: GetServerSideProps = async ({req}) => {
 
     const session = await getSession({req});
-    
-    //console.log(session)
 
     if(!session?.user) {
         return {
