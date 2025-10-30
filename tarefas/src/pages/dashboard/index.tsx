e502f8959d50d70b82d55caa97c8de7992dc8d59
leandrogarcia <leandro.oliveira.garcia@gmail.com>
2025-10-29 23:21:54 -0300

    feat: listing tasks

--- tarefas/src/pages/dashboard/index.tsx
+++ tarefas/src/pages/dashboard/index.tsx
@@ -5,9 +5,9 @@ import { getSession } from 'next-auth/react';
 import { Textarea } from '@/components/textarea';
 import {FiShare2} from 'react-icons/fi';
 import {FaTrash} from 'react-icons/fa';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { db } from '../../services/firebaseConnection';
-import { addDoc, collection} from 'firebase/firestore';
+import { addDoc, collection, query, orderBy, where, onSnapshot} from 'firebase/firestore';
 
 interface HomeProps{
     user: {
@@ -15,10 +15,49 @@ interface HomeProps{
     }
 }
 
+interface TaskProps {
+    id: string;
+    created: Date;
+    public: boolean;
+    tarefa: string;
+    user: string
+}
+
 export default function Dashboard({ user }: HomeProps){
 
     const [input, setInput] = useState('');
     const [publicTask, setPublicTask] = useState(false);
+    const [tasks, setTasks] = useState<TaskProps[]>([]);
+
+    useEffect(() => {
+        async function loadTasks() {
+            
+            const tarefasRef = collection(db, 'tarefas');
+            const q = query(
+                tarefasRef,
+                orderBy("created", 'desc'),
+                where("user", "==", user?.email)
+            );
+
+            onSnapshot(q, (snapshot) => {
+                let lista = [] as TaskProps[];
+
+                snapshot.forEach((doc) => {
+                    lista.push({
+                        id: doc.id,
+                        tarefa: doc.data().tarefa,
+                        created: doc.data().created,
+                        user: doc.data().user,
+                        public: doc.data().public
+                    });
+                });
+
+                setTasks(lista);
+            })
+        }
+
+        loadTasks();
+    }, [user?.email]);
 
     function handleChangePublic(event: ChangeEvent<HTMLInputElement>){
         setPublicTask(event.target.checked)
@@ -81,21 +120,29 @@ export default function Dashboard({ user }: HomeProps){
 
                 <section className={styles.taskContainer}>
                     <h1>Minhas tarefas</h1>
-                    <article className={styles.task}>
-                        <div className={styles.tagContainer}>
-                            <label className={styles.tag}>PUBLICO</label>
-                            <button className={styles.shareButton}>
-                                <FiShare2
-                                    size={22}
-                                    color='#3183ff'
-                                />
-                            </button>
-                        </div>
-                        <div className={styles.taskContent}> 
-                            <p>Minha primeira tarefa de exmplo</p>
-                            <button className={styles.trashButton}><FaTrash size={24} color='#ea3140' /></button>
-                        </div>
-                    </article>
+                    
+                    {
+                        tasks.map((item) => (
+                            <article key={item.id} className={styles.task}>
+                                {item.public && (
+                                    <div className={styles.tagContainer}>
+                                        <label className={styles.tag}>PUBLICO</label>
+                                        <button className={styles.shareButton}>
+                                            <FiShare2
+                                                size={22}
+                                                color='#3183ff'
+                                            />
+                                        </button>
+                                    </div>
+                                )}
+                                <div className={styles.taskContent}> 
+                                    <p>{item.tarefa}</p>
+                                    <button className={styles.trashButton}><FaTrash size={24} color='#ea3140' /></button>
+                                </div>
+                            </article>
+                        ))
+                    }
+
                     
                 </section>
             </main>
