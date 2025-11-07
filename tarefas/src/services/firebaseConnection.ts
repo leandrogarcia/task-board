fc6c8ebcf52a3d6179c0a54c549f290f7d01e2b7
leandrogarcia <leandro.oliveira.garcia@gmail.com>
2025-11-06 22:37:54 -0300

    feat: remove sensitive files

---
+++ tarefas/src/services/firebaseConnection.ts
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export { db };
